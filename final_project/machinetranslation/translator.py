9c8635045cb84942e91abedade4300cb1b3dc08c
ozdemirozcelik <ozdemirozcelik@gamil.com>
2023-01-30 17:23:48 -0500

    save changes

--- final_project/machinetranslation/translator.py
+++ final_project/machinetranslation/translator.py
@@ -8,3 +8,32 @@ load_dotenv()
 
 apikey = os.environ['apikey']
 url = os.environ['url']
+
+
+# translator_instance
+
+authenticator = IAMAuthenticator(apikey)
+language_translator = LanguageTranslatorV3(
+    version='2018-05-01',
+    authenticator=authenticator
+)
+
+language_translator.set_service_url(url)
+
+# e2f_translator_function
+
+def englishToFrench(englishText):
+    translation = language_translator.translate(
+    frenchText=englishText,
+    model_id='en-fr').get_result()
+    print(json.dumps(translation, indent=2, ensure_ascii=False))
+    return frenchText
+
+# f2e_translator_function
+
+def frenchToEnglish(frenchText):
+    translation = language_translator.translate(
+    englishText=frenchText,
+    model_id='fr-en').get_result()
+    print(json.dumps(translation, indent=2, ensure_ascii=False))
+    return englishText
